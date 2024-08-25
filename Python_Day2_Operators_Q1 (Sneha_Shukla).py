5cfbf0e5052b35b641f94294e0236d0264f3f21f
Sneha Shukla <78254975+SnehaShukla14@users.noreply.github.com>
2024-08-25 14:32:02 +0530

    Add files via upload

---
+++ Python_Day2_Operators_Q1 (Sneha_Shukla).py
@@ -0,0 +1,12 @@
+# 1. Calculate the multiplication and sum of two numbers
+
+a = 4
+b = 5
+c = 6
+d = 7
+print(" Multiplication of a and b is:", a*b)
+print(" Sum of c and d is:", c+d)
+
+# OUTPUT:
+# Multiplication of a and b is: 20
+# Sum of c and d is: 13
---
+++ Python_Day2_Operators_Q2_(Sneha_Shukla).py
@@ -0,0 +1,14 @@
+#2. Declare two variables and print that which variable is largest using ternary operators
+
+# Declare two variables
+a = 10
+b = 20
+
+# Use a ternary operator to find the largest variable
+largest = a if a > b else b
+
+# Print the largest variable
+print("The largest variable is:", largest)
+
+# OUTPUT:
+# The largest variable is: 20
