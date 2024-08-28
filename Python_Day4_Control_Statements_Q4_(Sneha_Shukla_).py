4dae8fe479df7fe0dda8d41fd2baa4f00431ad66
Sneha Shukla <78254975+SnehaShukla14@users.noreply.github.com>
2024-08-28 10:19:48 +0530

    Add files via upload

---
+++ Python_Day4_Control_Statements_Q4_(Sneha_Shukla_).py
@@ -0,0 +1,27 @@
+# Q.4) Create a Python program that checks if a user-given number is positive, negative, or zero.
+
+# CODE:
+    
+# Get the number from the user
+number = float(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
+
+# OUTPUT:
+
+# Enter a number: 5
+# The number is positive.
+
+# Enter a number: -92
+# The number is negative.
+
+# Enter a number: 0
+# The number is zero.
+
+
