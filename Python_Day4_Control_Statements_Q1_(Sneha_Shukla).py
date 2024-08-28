68ca99a089f05adb792c2729bedba6b096258fce
Sneha Shukla <78254975+SnehaShukla14@users.noreply.github.com>
2024-08-28 10:18:31 +0530

    Add files via upload

---
+++ Python_Day4_Control_Statements_Q1_(Sneha_Shukla).py
@@ -0,0 +1,21 @@
+# Q.1) Write a Python program that takes a number as input and prints "Even" if the number is even and "Odd" if it's odd.
+
+# CODE:
+
+# Get the number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# OUTPUT:
+
+# Enter a number: 34
+# Even
+
+# Enter a number: 45
+# Odd
+    
