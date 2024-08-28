188601e0046a1fdeff5b5fd4d6c1d93b30d28e81
Sneha Shukla <78254975+SnehaShukla14@users.noreply.github.com>
2024-08-28 18:42:03 +0530

    Add files via upload

---
+++ Pytho n_Day5_Functions_Q4_(Sneha_Shukla).py
@@ -0,0 +1,17 @@
+# Q.4) Accept a name from the user and display that in lower case using lower() function.
+
+# CODE:
+
+# Accept a name from the user
+name = input("Enter your name: ")
+
+# Convert the name to lowercase
+name_lower = name.lower()
+
+# Display the name in lowercase
+print(f"Your name in lowercase is: {name_lower}")
+
+# OUTPUT:
+
+# Enter your name: SNEHA SHUKLA
+# Your name in lowercase is: sneha shukla
