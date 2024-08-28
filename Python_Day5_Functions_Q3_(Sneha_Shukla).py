603d0fa2ef5e9103823b6c99f1a1aadba06d1988
Sneha Shukla <78254975+SnehaShukla14@users.noreply.github.com>
2024-08-28 18:41:20 +0530

    Add files via upload

---
+++ Python_Day5_Functions_Q3_(Sneha_Shukla).py
@@ -0,0 +1,26 @@
+# Q.3) Using max() and min() functions display the maximum and minimum of 5 random numbers.
+
+# CODE:
+
+# Manually define 5 random numbers
+random_numbers = [10, 9, 12, 5, 6]
+
+# Display the numbers
+print(f"Random_Numbers: {random_numbers}")
+
+# Find the maximum and minimum values
+max_value = max(random_numbers)
+min_value = min(random_numbers)
+
+# Display the maximum and minimum values
+print(f"Maximum value: {max_value}")
+print(f"Minimum value: {min_value}")
+
+# OUTPUT:
+
+# Random_Numbers: [10, 9, 12, 5, 6]
+# Maximum value: 12
+# Minimum value: 5
+
+
+
