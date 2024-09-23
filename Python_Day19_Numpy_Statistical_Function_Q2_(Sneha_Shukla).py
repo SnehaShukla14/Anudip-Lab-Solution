cc81195ab9af66d713acf8016b155dff169a6dbc
Sneha Shukla <78254975+SnehaShukla14@users.noreply.github.com>
2024-09-23 23:45:34 +0530

    Add files via upload

---
+++ Python_Day19_Numpy_Statistical_Function_Q2_(Sneha_Shukla).py
@@ -0,0 +1,20 @@
+# Q.2) Compute the standard deviation of the NumPy array
+
+# Input: arr = [20, 2, 7, 1, 34]
+
+# CODE:
+
+# Import NumPy
+import numpy as np
+
+# Input array
+arr = np.array([20, 2, 7, 1, 34])
+
+# Compute the standard deviation of the array
+std_deviation = np.std(arr)
+
+# Output the standard deviation
+print("Standard Deviation of the array:", std_deviation)
+
+# OUTPUT:
+# Standard Deviation of the array: 12.576167937809991
